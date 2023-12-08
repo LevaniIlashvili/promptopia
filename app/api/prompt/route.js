2fc56a229c8fd8f466c3757a36d0d576c8b3ed30
LevaniIlashvili <levaniko900@gmail.com>
2023-12-08 14:37:20 +0400

    prompt feed

---
+++ app/api/prompt/route.js
@@ -0,0 +1,15 @@
+import { connectToDb } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async () => {
+  try {
+    await connectToDb();
+
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts, { status: 200 }));
+  } catch (error) {
+    return new Response(
+      JSON.stringify("Failed to fetch all prompts", { status: 500 })
+    );
+  }
+};
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+type Creator = {
+  email: string;
+  password: string;
+  _id: string;
+};
+
+type Prompt = {
+  _id: string;
+  prompt: string;
+  tag: string;
+  creator: Creator;
+};
+
+const PromptCardList = ({
+  data,
+  handleTagClick,
+}: {
+  data: Prompt[];
+  handleTagClick: any;
+}) => {
+  console.log(data);
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt: Prompt) => {
+        console.log(prompt);
+        return (
+          <PromptCard
+            key={prompt._id}
+            prompt={prompt}
+            handtleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPrompts(data);
+    };
+
+    fetchPrompts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
